1b918cc40c95ded2e1a0d18a0a06674231dafab1
Aslanov Nijad <aslanlinicad@gmail.com>
2022-08-25 18:12:05 -0700

    Comparator

--- Collections/src/main/java/ArrayListReview.java
+++ Collections/src/main/java/ArrayListReview.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ArrayListReview {
     public static void main(String[] args) {
@@ -12,9 +11,52 @@ public class ArrayListReview {
         students.add(new Student(2, "Said"));
         students.add(new Student(3, "Nick"));
         students.add(new Student(4, "Josh"));
+        System.out.println(students);
 
+
+        //For Loop with get(index)
+        for (int i = 0; i < students.size(); i++) System.out.println("Student " + students.get(i).id);
+        System.out.println("_________________________");
+
+        // 2. Iterator
+        //Forward Iteration
+        Iterator iter = students.listIterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+
+        // Backwards Iteration
+        while (((ListIterator<?>) iter).hasPrevious()) {
+            System.out.println(((ListIterator<?>) iter).previous());
+
+        }
+
+        // forEach Loop
+        for (Student student : students) System.out.println(student);
+
+
+        // 4. Lambda
+        System.out.println("Lambdaaaaaa");
+        students.forEach(student -> System.out.println(student));
+
+
+        // Sorting Elements in List
+        System.out.println("Descending sort ..............");
+        Collections.sort(students, new sortIdDesc());
         System.out.println(students);
 
 
     }
+
+
+    static class sortIdDesc implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.id - o1.id;
+        }
+    }
+
+
+
 }
